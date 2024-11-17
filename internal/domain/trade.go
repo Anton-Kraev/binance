c8ce6e43bcf55a14ce16db5da9d0aac8d70cb3a3
Anton Kraev <antonkraeww@gmail.com>
2024-11-17 19:05:31 +0300

    removed id trade field

--- internal/domain/trade.go
+++ internal/domain/trade.go
@@ -9,7 +9,6 @@ import (
 )
 
 type Trade struct {
-	ID       int64
 	Time     int64
 	Quantity float64
 }
@@ -25,7 +24,6 @@ func FromBinanceTrade(trade *binance.Trade) Trade {
 	}
 
 	return Trade{
-		ID:       trade.ID,
 		Time:     trade.Time,
 		Quantity: qty,
 	}
@@ -47,19 +45,17 @@ func FromBinanceTradeEvent(event *binance.WsTradeEvent) Trade {
 	}
 
 	return Trade{
-		ID:       event.TradeID,
 		Time:     event.TradeTime,
 		Quantity: price * qty,
 	}
 }
 
 func TradeFields() string {
-	return "ID,Time,Quantity"
+	return "Time,Quantity"
 }
 
 func (t Trade) String() string {
-	return fmt.Sprintf("%d,%s,%.0f",
-		t.ID,
+	return fmt.Sprintf("%s,%.0f",
 		time.UnixMilli(t.Time).Format("02.01.2006-15:04:05"),
 		t.Quantity,
 	)
@@ -71,14 +67,13 @@ func (t Trade) IsSameTrade(nextTrade Trade) bool {
 
 func (t Trade) Merge(nextTrade Trade) Trade {
 	return Trade{
-		ID:       t.ID,
 		Time:     t.Time,
 		Quantity: t.Quantity + nextTrade.Quantity,
 	}
 }
 
 func (t Trade) MatchesFilter(qtyThreshold float64) bool {
-	isValid := t.ID > 0 && t.Time > 0
+	isValid := t.Time > 0
 	isOverThreshold := t.Quantity > qtyThreshold
 
 	return isValid && isOverThreshold
